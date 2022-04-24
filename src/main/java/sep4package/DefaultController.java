601a89077f73d44ce8b00872dfeaa39fd91a5161
Iana01 <304119@via.dk>
2022-04-24 20:03:56 +0200

    ci set up

--- src/main/java/sep4package/DefaultController.java
+++ src/main/java/sep4package/DefaultController.java
@@ -8,6 +8,6 @@ public class DefaultController {
 
     @GetMapping("/")
     String health() {
-        return "This is the cloud workshop app modified for sep4";
+        return "This is the workshop app modified for sep4";
     }
 }
