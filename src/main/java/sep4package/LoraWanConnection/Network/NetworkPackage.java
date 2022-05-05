78b17e4a67f2d111ca15a9bc94e22f66fa66e88c
Iana01 <304119@via.dk>
2022-05-05 12:40:16 +0200

    gson.jar + no comments

--- src/main/java/sep4package/LoraWanConnection/Network/NetworkPackage.java
+++ src/main/java/sep4package/LoraWanConnection/Network/NetworkPackage.java
@@ -1,46 +1,24 @@
 package sep4package.LoraWanConnection.Network;
 
-/**
- * The type Network package.
- */
+
 public class NetworkPackage {
   private String Sensors = "Sensors";
   private Object object;
 
-  /**
-   * Instantiates a new Network package.
-   *
-   * @param type   the type
-   * @param object the object
-   */
   public NetworkPackage(String type, Object object) {
     Sensors = type;
     this.object = object;
   }
 
-  /**
-   * Gets type.
-   *
-   * @return the type
-   */
   public String getType() {
     return Sensors;
   }
 
-  /**
-   * Gets object.
-   *
-   * @return the object
-   */
+
   public Object getObject() {
     return object;
   }
 
-  /**
-   * Sets object.
-   *
-   * @param object the object
-   */
   public void setObject(Object object) {
     this.object = object;
   }
