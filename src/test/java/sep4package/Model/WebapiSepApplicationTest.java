2740d49ccf7e58a0386d56e68a10bbe5849bb38a
Iana01 <304119@via.dk>
2022-05-12 15:15:00 +0200

    and again

--- src/test/java/sep4package/Model/WebapiSepApplicationTest.java
+++ src/test/java/sep4package/Model/WebapiSepApplicationTest.java
@@ -4,16 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories
 class WebapiSepApplicationTest
 {
-  private final SensorsRepository repository;
-  public WebapiSepApplicationTest(SensorsRepository repository)
-  {
-    this.repository = repository;
-  }
-
   @Test
   void contextLoads() {
   }
