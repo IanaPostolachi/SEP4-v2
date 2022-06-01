302838439f3e0efa1ab59547c1c61d1a99e4d08f
Siyu <1021109492@qq.com>
2022-06-01 10:33:23 +0200

    latest problem-free version

--- src/main/java/sep4package/Model/Windows/ReceiveWindow.java
+++ src/main/java/sep4package/Model/Windows/ReceiveWindow.java
@@ -1,7 +1,5 @@
 package sep4package.Model.Windows;
 
-import sep4package.LoraWanConnection.Service.WindowStatus;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
